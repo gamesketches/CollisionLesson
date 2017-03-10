3c7af50a9e6d1045f22a39931e791418631be2af
gamesketches <sam.von.ehren@gmail.com>
2017-03-10 15:48:50 -0500

    Added in a bit more logic

---
+++ Assets/AttackerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerScript : MonoBehaviour {
+
+	public bool AddRigidbody;
+	private bool moving;
+	// Use this for initialization
+	void Start () {
+		if(AddRigidbody) {
+			gameObject.AddComponent<Rigidbody>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			if(AddRigidbody) {
+				gameObject.GetComponent<Rigidbody>().AddForce(1000, 0, 0);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		
+	}
+
+	void OnTriggerStay(Collider other) {
+	
+	}
+
+	void OnTriggerExit(Collider other) {
+
+	}
+
+	void OnCollisionEnter(Collision col) {
+
+	}
+
+	void OnCollisionStay(Collision col) {
+
+	}
+
+	void OnCollisionExit(Collision col) {
+
+	}
+}
---
+++ Assets/DefenderScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderScript : MonoBehaviour {
+
+	public int framesOnEnter;
+	public int framesOnStay;
+	public int framesOnExit;
+	// Use this for initialization
+	void Start () {
+		framesOnEnter = 0;
+		framesOnStay = 0;
+		framesOnExit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		framesOnEnter++;
+	}
+
+	void OnTriggerStay(Collider other) {
+		framesOnStay++;
+	}
+
+	void OnTriggerExit(Collider other) {
+		framesOnExit++;
+	}
+
+	void OnCollisionEnter(Collision col) {
+		Destroy(col.gameObject);
+	}
+
+	void OnCollisionStay(Collision col) {
+
+	}
+
+	void OnCollisionExit(Collision col) {
+
+	}
+}
